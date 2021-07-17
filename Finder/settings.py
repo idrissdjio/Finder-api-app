29651b76208cb0e788eb8db0831a6186ad8f39fe
idrissdjio <djiofackidriss@gmail.com>
2021-07-17 23:49:22 +0800

    added static root

--- Finder/settings.py
+++ Finder/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
