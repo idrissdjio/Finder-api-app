0fbf980567b10506a9bf7946c52c616b49cadfe4
idrissdjio <djiofackidriss@gmail.com>
2021-07-27 20:56:13 +0800

    removed cloudinary

--- Finder/settings.py
+++ Finder/settings.py
@@ -17,9 +17,9 @@ from pathlib import Path
 import os
 
 
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+# import cloudinary
+# import cloudinary.uploader
+# import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
